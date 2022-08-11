0176c19b708a432b211c2ee4d825f4f2a4c345df
robloxscriptleaks <98795870+robloxscriptleaks@users.noreply.github.com>
2022-08-11 15:03:38 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
-local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/robloxscriptleaks/-NiggaFactory/main/%F0%9F%98%82", true))()
+local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/robloxscriptleaks/-NiggaFactory/main/WL.lua", true))()
 if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/robloxscriptleaks/WILLSOPAPESHIT/main/MonkeyHub.lua", true))()
 else
